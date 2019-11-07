23d41c2a4c68fae5109782511b55b6d2fa13572a
Hanna Farag <hannafarag159@gmail.com>
2019-11-07 13:38:29 -0800

    Redirect user to login if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
 
+# Redirect to jobs pages when user signed in
   def after_sign_in_path_for(resource)
-    jobs_path #your path
+    jobs_path
+  end
+
+  # Redirect to root path if user has not signed in
+  protected
+  def authenticate_user!
+    redirect_to root_path, notice: "You must login" unless (employer_signed_in? || candidate_signed_in?)
   end
 
 end
